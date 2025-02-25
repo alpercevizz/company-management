6d731f36153fea9429835b2c3221f7f88cbc53c4
Alper Ceviz <64170437+alpercevizz@users.noreply.github.com>
2025-02-25 16:58:04 +0300

    update company.py

--- company.py
+++ company.py
@@ -11,7 +11,13 @@ class Company:
 
         try:
             with open(self.db_file, "r", encoding="utf-8") as file:
-                return json.load(file)
+                data = json.load(file)
+
+                if isinstance(data, list):
+                    return data
+                else:
+                    return []
+
         except (FileNotFoundError, json.JSONDecodeError):
             return []
         
@@ -45,6 +51,45 @@ class Company:
                 return
         print("Employee not found!")
 
+    def delete_employee(self, emp_id):
+        """Çalışanları ID'ye göre silme"""
+        for emp in self.employees:
+            if emp["emp_id"] == emp_id:
+                self.employees.remove(emp)
+                self.save_data()
+                print(f"Employee with ID {emp_id} deleted successfully!")
+                return
+        print("Employee not found.")
+    
+    def search_employee(self, keyword):
+        """İsme veya ID'ye göre arama"""
+
+        found = False
+
+        for emp in self.employees:
+            if keyword.lower() in emp["name"].lower() or str(emp["emp_id"]) == str(keyword):
+                print(f"Found employee: {emp}")
+                found = True
+        if not found:
+            print("No employee found with given keyword!")
+    
+    def list_by_department(self, department):
+        """Belirtilen departmandaki yöneticileri listeler"""
+
+        print(f"\n Employees in {department} Department")
+
+        for emp in self.employees:
+            if "department" in emp and emp["department"].lower() == department.lower():
+                print(f"ID: {emp['emp_id']}, Name: {emp['name']}, Email: {emp['email']}")
+
+    def company_statistics(self):
+        """Toplam çalışan sayısını ve maaş ortalamasını hesapla"""
+
+        total_salary = sum(emp["salary"] for emp in self.employees)
+        avg_salary = total_salary / len(self.employees) if self.employees else 0
+        print(f"Total employees: {len(self.employees)}")
+        print(f"Average salary: {avg_salary:.2f} TRY")
+
 company = Company()
 emp1 = Employee(1, "Alper", 28, "alper@alper.com", 48000)
 mgr1 = Manager(2, "Can", 40, "can@can.com", 85000, "IT")
@@ -66,4 +111,16 @@ company.update_salary(3, 60000)
 # Güncellenmiş liste 
 
 company.list_employees()
-        
+
+# Çalışanları Silme
+company.delete_employee()
+
+# Çalışan Arama
+company.search_employee()
+
+# Çalışan Listeleme
+company.list_by_department("IT") # IT departmanındaki yöneticiler
+
+# Çalışan Sayısı ve Maaş Ortalaması 
+
+company.company_statistics()
